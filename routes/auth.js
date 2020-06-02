fe15b0de73accfd7b18024a702d04e7b7873c487
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-06-01 22:49:49 -0400

    arquitecture rest server

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    signup,
+    signin,
+    signout,
+    requireSignIn
+} = require("../controllers/auth");
+
+const {userSignupValidator} = require("../validator");
+
+// define routes
+
+router.post("/signup", userSignupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
+
+module.exports = router;
